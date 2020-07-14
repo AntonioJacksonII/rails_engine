399d7cabda86b622bdb1e7d2bd7bfdd8085ac446
Antonio Jackson <antoniojackson@gmail.com>
2020-07-14 16:04:18 -0600

    Creating customers table and Customer model with associated model test for Customer

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Customer, type: :model do
+  it 'exists and has attributes' do
+    attributes = {
+      first_name: 'Joey',
+      last_name: 'Ondricka',
+      created_at: '2012-03-27 14:54:09 UTC',
+      updated_at: '2012-03-27 14:54:09 UTC'
+    }
+
+    customer = Customer.new(attributes)
+
+    expect(customer).to be_a Customer
+    expect(customer.first_name).to eq('Joey')
+    expect(customer.last_name).to eq('Ondricka')
+    expect(customer.created_at).to eq('2012-03-27 14:54:09 UTC')
+    expect(customer.updated_at).to eq('2012-03-27 14:54:09 UTC')
+  end
+end
