afa4010a5f9a4e5d484cb796f977f7cec133deff
Antonio Jackson <antoniojackson@gmail.com>
2020-07-14 19:35:59 -0600

    Creating merchants table with associated model test

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Merchant, type: :model do
+  it 'exists and has attributes' do
+    attributes = {
+      name: 'Schroeder-Jerde',
+      created_at: '2012-03-27 14:53:59 UTC',
+      updated_at: '2012-03-27 14:53:59 UTC'
+    }
+
+    merchant = Merchant.new(attributes)
+
+    expect(merchant).to be_a Merchant
+    expect(merchant.name).to eq('Schroeder-Jerde')
+    expect(merchant.created_at).to eq('2012-03-27 14:53:59 UTC')
+    expect(merchant.updated_at).to eq('2012-03-27 14:53:59 UTC')
+  end
+end
