b1a5a336f536bbc3eb73e77977dc60012b97dc81
Antonio Jackson <antoniojackson@gmail.com>
2020-07-15 03:03:18 -0600

    Adding show, create, and update actions to merchants_controller with associated requests tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,23 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    id = params[:id]
+    render json: MerchantSerializer.new(Merchant.find(id))
+  end
+
+  def create
+    render json: MerchantSerializer.new(Merchant.create(merchant_params))
+  end
+
+  def update
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
+  end
+
+  private
+
+    def merchant_params
+      params.permit(:name)
+    end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,4 +10,33 @@ describe 'Merchants API' do
     merchants = JSON.parse(response.body, symbolize_names: true)
     expect(merchants[:data].count).to eq(3)
   end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(merchant[:data][:id]).to eq(id.to_s)
+  end
+
+  it 'can create a new merchant' do
+    post '/api/v1/merchants', params: {name: 'Test Merchant'}
+    merchant = Merchant.last
+
+    expect(response).to be_successful
+    expect(merchant.name).to eq('Test Merchant')
+  end
+
+  it 'can update a merchant' do
+    id = create(:merchant).id
+    previous_name = Merchant.last.name
+    patch "/api/v1/merchants/#{id}", params: { name: 'New Merchant Name' }
+
+    merchant = Merchant.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(merchant.name).to_not eq(previous_name)
+    expect(merchant.name).to eq('New Merchant Name')
+  end
 end
