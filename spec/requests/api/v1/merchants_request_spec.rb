d2b1421e30bd58116aa468dff462ef18f7895e6d
Antonio Jackson <antoniojackson@gmail.com>
2020-07-15 01:45:52 -0600

    Adding merchants index route with associated test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 describe 'Merchants API' do
   it 'sends a list of merchants' do
     create_list(:merchant, 3)
-
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(3)
   end
 end
