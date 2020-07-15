6a26334c32473be594a6f44cc95941f9db4714da
Antonio Jackson <antoniojackson@gmail.com>
2020-07-14 23:24:13 -0600

    Updating credit_card_number to string from integer

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,7 +4,7 @@ describe Transaction, type: :model do
   it 'exists and has attributes' do
     attributes = {
       invoice_id: 1,
-      credit_card_number: 4654405418249632,
+      credit_card_number: '4654405418249632',
       result: 'success',
       created_at: '2012-03-27 14:54:09 UTC',
       updated_at: '2012-03-27 14:54:09 UTC'
@@ -14,7 +14,7 @@ describe Transaction, type: :model do
 
     expect(transaction).to be_a Transaction
     expect(transaction.invoice_id).to eq(1)
-    expect(transaction.credit_card_number).to eq(4654405418249632)
+    expect(transaction.credit_card_number).to eq('4654405418249632')
     expect(transaction.credit_card_expiration_date).to eq(nil)
     expect(transaction.result).to eq('success')
     expect(transaction.created_at).to eq('2012-03-27 14:54:09 UTC')
