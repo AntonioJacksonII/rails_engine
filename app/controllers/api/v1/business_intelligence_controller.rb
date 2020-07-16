91d4ae2dd7c63d5333907d8dc14e95ea05b21bd8
Antonio Jackson <antoniojackson@gmail.com>
2020-07-16 06:13:10 -0600

    Adding route and updating business intelligence controller to include most items sold functionality

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::BusinessIntelligenceController < ApplicationController
   def index
-    if @_request.path == '/api/v1/merchants/most_revenue'
-      quantity = params[:quantity].to_i
+    quantity = params[:quantity].to_i
+    if request.path == '/api/v1/merchants/most_revenue'
       data = Merchant.joins(invoices:[:invoice_items, :transactions]).where("transactions.result='success'").group("merchants.id").select("merchants.id, merchants.name, sum(invoice_items.quantity*invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
+      render json: MerchantSerializer.new(data)
+    elsif request.path == '/api/v1/merchants/most_items'
+      data = data = Merchant.joins(invoices:[:invoice_items, :transactions]).where("transactions.result='success'").group("merchants.id").select("merchants.id, merchants.name, sum(invoice_items.quantity) as total_sold").order("total_sold desc").limit(quantity)
       render json: MerchantSerializer.new(data)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/merchants/find', to: 'merchants_search#show'
       get '/merchants/find_all', to: 'merchants_search#index'
       get '/merchants/most_revenue', to: 'business_intelligence#index'
+      get '/merchants/most_items', to: 'business_intelligence#index'
       resources :merchants do
         resources :items, only: [:index]
       end
