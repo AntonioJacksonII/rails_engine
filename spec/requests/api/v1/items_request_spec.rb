73e9d8e801522f8bad1c5d29f47e96c7f824eaa0
Antonio Jackson <antoniojackson@gmail.com>
2020-07-16 07:45:59 -0600

    Adding items show path

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -15,4 +15,15 @@ describe 'Items API' do
     expect(items[:data].count).to eq(3)
     expect(items[:data]).to_not be_empty
   end
+
+  it 'can get one item by its id' do
+    create :merchant
+    merchant = Merchant.last
+    id = create(:item, merchant: merchant).id
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(item[:data][:id]).to eq(id.to_s)
+  end
 end
