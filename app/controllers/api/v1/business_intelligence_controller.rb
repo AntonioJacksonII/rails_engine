b1dbc9669c37f73253e7a951de2c9f806aa1fcf0
Antonio Jackson <antoniojackson@gmail.com>
2020-07-16 06:00:56 -0600

    Updating business intelligence most revenue AR query to order by merchant_id and also select merchant_name

--- app/controllers/api/v1/business_intelligence_controller.rb
+++ app/controllers/api/v1/business_intelligence_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::BusinessIntelligenceController < ApplicationController
   def index
     if @_request.path == '/api/v1/merchants/most_revenue'
       quantity = params[:quantity].to_i
-      data = Merchant.joins(invoices:[:invoice_items, :transactions]).where("transactions.result='success'").group("merchants.name").select("merchants.name, sum(invoice_items.quantity*invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
+      data = Merchant.joins(invoices:[:invoice_items, :transactions]).where("transactions.result='success'").group("merchants.id").select("merchants.id, merchants.name, sum(invoice_items.quantity*invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
       render json: MerchantSerializer.new(data)
     end
   end
