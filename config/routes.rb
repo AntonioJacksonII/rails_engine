958c9dca231e38d98091102e31accb6907e21ae9
Antonio Jackson <antoniojackson@gmail.com>
2020-07-16 07:26:47 -0600

    Adding merchant revenue business intelligence route and updating business intelligence controller to include merchant revenue functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/merchants/find', to: 'merchants_search#show'
       get '/merchants/find_all', to: 'merchants_search#index'
+
       get '/merchants/most_revenue', to: 'business_intelligence#index'
       get '/merchants/most_items', to: 'business_intelligence#index'
+      get '/merchants/:id/revenue', to: 'business_intelligence#index'
+
       resources :merchants do
         resources :items, only: [:index]
       end
